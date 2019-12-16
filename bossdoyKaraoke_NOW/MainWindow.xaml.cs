bfbe1acac2e4dbde01a6803c4b1f452c34fdd6f3
TanieSC <38749767+TanieSC@users.noreply.github.com>
2019-12-16 14:43:56 +0800

    Update README.md

--- bossdoyKaraoke_NOW/MainWindow.xaml.cs
+++ bossdoyKaraoke_NOW/MainWindow.xaml.cs
@@ -37,7 +37,6 @@ namespace bossdoyKaraoke_NOW
         private const int WM_DISPLAYCHANGE = 0x7E;
         private Player _player;
         private VideoImage _videoImage;
-        //GlobalHotkeyService _ctlO;
 
         public MainWindow()
         {
@@ -65,32 +64,12 @@ namespace bossdoyKaraoke_NOW
             SystemState.KeepDisplayActive();
         }
 
-        //[DllImport("user32.dll")]
-        //private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
-
-        //[DllImport("user32.dll")]
-        //private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
-        //private const int HOTKEY_ID = 9000;
-
-        ////Modifiers:
-        //private const uint MOD_NONE = 0x0000; //(none)
-        //private const uint MOD_ALT = 0x0001; //ALT
-        //private const uint MOD_CONTROL = 0x0002; //CTRL
-        //private const uint MOD_SHIFT = 0x0004; //SHIFT
-        //private const uint MOD_WIN = 0x0008; //WINDOWS
-        ////CAPS LOCK:
-        //private const uint VK_CAPITAL = 0x14;
-
         private HwndSource _source;
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
             _source = PresentationSource.FromVisual(this) as HwndSource;
             _source.AddHook(WndProc);
-
-           // RegisterHotKey(_source.Handle, HOTKEY_ID, MOD_CONTROL, VK_CAPITAL); //CTRL + CAPS_LOCK
-           // RegisterHotKey(_source.Handle, 1, MOD_CONTROL, 0x45);
         }
 
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
@@ -105,31 +84,6 @@ namespace bossdoyKaraoke_NOW
                 int monCount = ScreenInformation.GetMonitorCount();
                // int winFormsMonCount = System.Windows.Forms.Screen.AllScreens.Length;
             }
-
-            //const int WM_HOTKEY = 0x0312;
-            //switch (msg)
-            //{
-            //    case WM_HOTKEY:
-            //        switch (wParam.ToInt32())
-            //        {
-            //            case HOTKEY_ID:
-            //                int vkey = (((int)lParam >> 16) & 0xFFFF);
-            //                if (vkey == VK_CAPITAL)
-            //                {
-            //                    Console.WriteLine("CapsLock was pressed");
-            //                }
-            //                handled = true;
-            //                break;
-            //            case 1:
-            //                int vkeys = (((int)lParam >> 16) & 0xFFFF);
-            //                if (vkeys == 0x45)
-            //                {
-            //                    Console.WriteLine("E was pressed");
-            //                }
-            //                    break;
-            //        }
-            //        break;
-            //}
 
             return IntPtr.Zero;
         }
